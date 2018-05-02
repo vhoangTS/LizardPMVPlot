edcfeee96fefa6c37e658d33f6118743443bd4ef
vhoang <levy.2807@gmail.com>
2018-05-02 11:26:47 +0200

    Formatting

--- PMVPlotting.py
+++ PMVPlotting.py
@@ -10,136 +10,149 @@ color_SlightlyWarm = 'rgb(255,204,0)'
 color_Hot = 'rgb(255,111,71)'
 color_ExtremeHot = 'rgb(255,0,0)'
 
-def traceseries(colorPMV,xvalues,yvalues,matchcolor,name):
-    '''divided the color list to different list to use as different traces'''
+
+def traceseries(colorPMV, xvalues, yvalues, matchcolor, name):
+    """divided the color list to different list to use as different traces"""
     matchX = []
     matchY = []
-    for id,color in enumerate(colorPMV):
+    for iid, color in enumerate(colorPMV):
         if color == matchcolor:
-            matchX.append(xvalues[id])
-            matchY.append(yvalues[id])
+            matchX.append(xvalues[iid])
+            matchY.append(yvalues[iid])
     trace = go.Scatter(
-            name = name,
-            x = matchX,
-            y= matchY,
-            mode='markers',
-            marker=dict(
+        name=name,
+        x=matchX,
+        y=matchY,
+        mode='markers',
+        marker=dict(
             size='4',
-            color = matchcolor, #set color equal to a variable
+            color=matchcolor,  # set color equal to a variable
             showscale=False,
-            line = dict(width = 0.3, color = matchcolor)
-                        )
-                    )
+            line=dict(width=0.3, color=matchcolor)
+        )
+    )
     return trace
 
-def PMV_plotlyScatter(colorPMV,xvalues,yvalues,stat):
+
+def PMV_plotlyScatter(colorPMV, xvalues, yvalues, stat, pickedID):
     """Plotting yearly comfort values"""
-    Unoccupied = traceseries(colorPMV,xvalues,yvalues,color_Unoccupied,"Unoccupied")
-    ExtremeCold = traceseries(colorPMV,xvalues,yvalues,color_ExtremeCold,"Extreme Cold: %d hrs"%(stat[0]))
-    Cold = traceseries(colorPMV,xvalues,yvalues,color_Cold,"Cold: %d hrs"%(stat[1]))
-    SlightlyCold = traceseries(colorPMV,xvalues,yvalues,color_SlightlyCold,"Slightly Cold: %d hrs"%(stat[2]))
-    Comfortable = traceseries(colorPMV,xvalues,yvalues,color_Comfortable,"Comfortable: %d hrs"%(stat[3]))
-    SlightlyWarm = traceseries(colorPMV,xvalues,yvalues,color_SlightlyWarm,"Slightly Warm: %d hrs"%(stat[4]))
-    Hot = traceseries(colorPMV,xvalues,yvalues,color_Hot,"Hot: %d hrs"%(stat[5]))
-    ExtremeHot = traceseries(colorPMV,xvalues,yvalues,color_ExtremeHot,"Extreme Hot: %d hrs"%(stat[6]))
+    Unoccupied = traceseries(colorPMV, xvalues, yvalues, color_Unoccupied, "Unoccupied")
+    ExtremeCold = traceseries(colorPMV, xvalues, yvalues, color_ExtremeCold, "Extreme Cold: %d hrs" % (stat[0]))
+    Cold = traceseries(colorPMV, xvalues, yvalues, color_Cold, "Cold: %d hrs" % (stat[1]))
+    SlightlyCold = traceseries(colorPMV, xvalues, yvalues, color_SlightlyCold, "Slightly Cold: %d hrs" % (stat[2]))
+    Comfortable = traceseries(colorPMV, xvalues, yvalues, color_Comfortable, "Comfortable: %d hrs" % (stat[3]))
+    SlightlyWarm = traceseries(colorPMV, xvalues, yvalues, color_SlightlyWarm, "Slightly Warm: %d hrs" % (stat[4]))
+    Hot = traceseries(colorPMV, xvalues, yvalues, color_Hot, "Hot: %d hrs" % (stat[5]))
+    ExtremeHot = traceseries(colorPMV, xvalues, yvalues, color_ExtremeHot, "Extreme Hot: %d hrs" % (stat[6]))
 
-    data = [ExtremeHot,Hot,SlightlyWarm,Comfortable,SlightlyCold,Cold,ExtremeCold,Unoccupied]
+    data = [ExtremeHot, Hot, SlightlyWarm, Comfortable, SlightlyCold, Cold, ExtremeCold, Unoccupied]
     layout = go.Layout(
-        width= 1900,height = 330,
-        title = "Hourly Comfort",
-        xaxis = dict(
-            #fixedrange = True,
-            zeroline = False,
-            showline = False,
-            showgrid = False,
-            tick0 = 0,
-            dtick = "M1",
-            tickformat = "%b",
-            ticklen = 3,
-            tickwidth = 1,
-            ),
-        yaxis = dict(
-            #fixedrange = True,
-            autotick = False,
-            showgrid = False,
-            zeroline = False,
-            showline = False,
-            ticks = 'outside',
-            tick0 = 0,
-            dtick = 8,
-            ticklen = 0,
-            tickwidth = 1,
-            tickcolor = '#000'
+        width=1900, height=330,
+        title="Hourly Comfort",
+        xaxis=dict(
+            # fixedrange = True,
+            zeroline=False,
+            showline=False,
+            showgrid=False,
+            tick0=0,
+            dtick="M1",
+            tickformat="%b",
+            ticklen=3,
+            tickwidth=1,
+        ),
+        yaxis=dict(
+            # fixedrange = True,
+            autotick=False,
+            showgrid=False,
+            zeroline=False,
+            showline=False,
+            ticks='outside',
+            tick0=0,
+            dtick=8,
+            ticklen=0,
+            tickwidth=1,
+            tickcolor='#000'
         )
-        )
+    )
 
-    fig = go.Figure(data = data, layout= layout)
-    py.offline.plot(fig, filename='Yearly_PMV_ptsID.html', image_filename="Yearly_PMV_ptsID", image_width=1900,image_height=330) #image = 'png',
+    fig = go.Figure(data=data, layout=layout)
+    py.offline.plot(fig, filename='Yearly_PMV_ptsID_C%d.html' % pickedID,
+                    image_filename="Yearly_PMV_ptsID_C%d" % pickedID, image_width=1900,
+                    image_height=330)  # image = 'png',
 
-def PMV_BarStatID(pers,statname,statcolor,pickedID):
+
+def PMV_BarStatID(pers, statname, statcolor, pickedID):
     """Plotting comfort statistic of selected pickedID"""
-    def stattrace(pickedID,statvalue,color,name):
+
+    def stattrace(pickedid, statvalue, color, name):
         trace = go.Bar(
-            x= [pickedID],
-            y= [statvalue],
-            name= name,
-            width = [0.1],
-            text = name,
-            marker = dict(color = color),
-            )
+            x=[pickedid],
+            y=[statvalue],
+            name=name,
+            width=[0.1],
+            text=name,
+            marker=dict(color=color),
+        )
         return trace
-    excold = stattrace(pickedID,pers[0],statcolor[0],statname[0])
-    cold = stattrace(pickedID,pers[1],statcolor[1],statname[1])
-    slcold = stattrace(pickedID,pers[2],statcolor[2],statname[2])
-    comf = stattrace(pickedID,pers[3],statcolor[3],statname[3])
-    slwarm = stattrace(pickedID,pers[4],statcolor[4],statname[4])
-    hot = stattrace(pickedID,pers[5],statcolor[5],statname[5])
-    exhot = stattrace(pickedID,pers[6],statcolor[6],statname[6])
-    data = [excold,cold,slcold,comf,slwarm,hot,exhot]
-    layout = go.Layout(barmode = 'stack',title='PMV Statistic on C%s'%(str(pickedID)),width= 1920,height = 1080)
+
+    excold = stattrace(pickedID, pers[0], statcolor[0], statname[0])
+    cold = stattrace(pickedID, pers[1], statcolor[1], statname[1])
+    slcold = stattrace(pickedID, pers[2], statcolor[2], statname[2])
+    comf = stattrace(pickedID, pers[3], statcolor[3], statname[3])
+    slwarm = stattrace(pickedID, pers[4], statcolor[4], statname[4])
+    hot = stattrace(pickedID, pers[5], statcolor[5], statname[5])
+    exhot = stattrace(pickedID, pers[6], statcolor[6], statname[6])
+    data = [excold, cold, slcold, comf, slwarm, hot, exhot]
+    layout = go.Layout(barmode='stack', title='PMV Statistic on C%s' % (str(pickedID)), width=1920, height=1080)
     fig = go.Figure(data=data, layout=layout)
     py.offline.plot(fig, filename='PMV_Stat_PtsID.html')
 
-def PMV_BarStatALL(statdict,statname,statcolor):
+
+def PMV_BarStatALL(statdict, statname, statcolor):
     """Plotting comfort statistic of all comfortID"""
-    def traceassign(statdict,statname,nameID):
+
+    def traceassign(statdict, statname, nameID):
         xtrace = []
         ytrace = []
         for key in statdict.keys():
             xtrace.append(key)
             ytrace.append(statdict[key][nameID])
         trace = go.Bar(
-            x= xtrace,
-            y= ytrace,
-            #width = [0.1],
-            name= statname[nameID],
-            marker = dict(color = statcolor[nameID]))
+            x=xtrace,
+            y=ytrace,
+            # width = [0.1],
+            name=statname[nameID],
+            marker=dict(color=statcolor[nameID]))
         return trace
-    excold = traceassign(statdict,statname,0)
-    cold = traceassign(statdict,statname,1)
-    slcold = traceassign(statdict,statname,2)
-    comf =  traceassign(statdict,statname,3)
-    slwarm =  traceassign(statdict,statname,4)
-    hot =  traceassign(statdict,statname,5)
-    exhot =  traceassign(statdict,statname,6)
-    data = [excold,cold,slcold,comf,slwarm,hot,exhot]
+
+    excold = traceassign(statdict, statname, 0)
+    cold = traceassign(statdict, statname, 1)
+    slcold = traceassign(statdict, statname, 2)
+    comf = traceassign(statdict, statname, 3)
+    slwarm = traceassign(statdict, statname, 4)
+    hot = traceassign(statdict, statname, 5)
+    exhot = traceassign(statdict, statname, 6)
+    data = [excold, cold, slcold, comf, slwarm, hot, exhot]
     layout = go.Layout(
-                    barmode = 'stack',
-                    title='PMV Statistic',
-                    width= 1920,height = 1080)
+        barmode='stack',
+        title='PMV Statistic',
+        width=1920, height=1080)
     fig = go.Figure(data=data, layout=layout)
     py.offline.plot(fig, filename='PMV_Stat_All.html')
 
-def PMV_3DStatScatter(statdict,comfortpts):
+
+def PMV_3DStatScatter(statdict, comfortpts):
     """3D plotting based on comfort pts X,Y coordinate and Z is statistic of comfortable hours during selected period"""
+
     def getXY(comfortpts):
-        ptsX, ptsY = [],[]
+        ptsx, ptsy = [], []
         for pts in comfortpts.keys():
-            ptsX.append(comfortpts[pts][0])
-            ptsY.append(comfortpts[pts][1])
-        return ptsX,ptsY
-    def getStatlistasZ(statdict,statname):
-        #statname = ['Extreme Cold', 'Cold', 'Slightly Cold', 'Comfortable', 'Slightly Warm','Hot','Extreme Hot']
+            ptsx.append(comfortpts[pts][0])
+            ptsy.append(comfortpts[pts][1])
+        return ptsx, ptsy
+
+    def getStatlistasZ(statdict, statname):
+        # statname = ['Extreme Cold', 'Cold', 'Slightly Cold', 'Comfortable', 'Slightly Warm','Hot','Extreme Hot']
         if statname == 'Extreme Cold':
             statID = 0
         elif statname == 'Cold':
@@ -159,24 +172,24 @@ def PMV_3DStatScatter(statdict,comfortpts):
             Z3DScatter.append(statdict[pts][statID])
         return Z3DScatter
 
-    ptsX,ptsY = getXY(comfortpts)
-    ptsZ = getStatlistasZ(statdict,"Comfortable")
+    ptsX, ptsY = getXY(comfortpts)
+    ptsZ = getStatlistasZ(statdict, "Comfortable")
     trace = go.Scatter3d(
-        x = ptsX,
-        y = ptsY,
-        z = ptsZ,
-        mode = 'markers',
-        marker = dict(
-            size = 8,
-            color = 'rgb(127, 127, 127)',
-            colorscale = 'Viridis',
-            opacity = 1)
-        )
+        x=ptsX,
+        y=ptsY,
+        z=ptsZ,
+        mode='markers',
+        marker=dict(
+            size=8,
+            color='rgb(127, 127, 127)',
+            colorscale='Viridis',
+            opacity=1)
+    )
     data = [trace]
     layout = go.Layout(
-        margin = dict(l=0,r=0,b=0,t=0),
-        #zaxis = dict(autorange = False)
-        xaxis = dict(autorange = False)
+        margin=dict(l=0, r=0, b=0, t=0),
+        # zaxis = dict(autorange = False)
+        xaxis=dict(autorange=False)
     )
-    fig = go.Figure(data = data, layout = layout)
+    fig = go.Figure(data=data, layout=layout)
     py.offline.plot(fig, filename='PMV_Stat_3D.html')
--- fileReader.py
+++ fileReader.py
@@ -4,20 +4,21 @@ import plotly as py
 import plotly.graph_objs as go
 import datetime
 
-#if working from office, use this path
-b18file = 'c:\\Users\\vhoang\\Desktop\\_TEMP\Model\\BASIS\\BASIS.b18'
-temperaturePRN = 'c:\\Users\\vhoang\\Desktop\\_TEMP\\Model\\BASIS\\Results\\temp_1h_Z1.prn'
-comfortPRN = 'c:\\Users\\vhoang\\Desktop\\_TEMP\\Model\\BASIS\\Results\\Comfort_1h.prn'
+# if working from office, use this path
+b18file = 'p:\\_Akquise\\Stuttgart_KnippersHelbig\\TRNLizSim\\Model\\V4_ShadingAndScreedAndFan\\V4_ShadingAndScreedAndFan.b18'
+temperaturePRN = 'p:\\_Akquise\\Stuttgart_KnippersHelbig\\TRNLizSim\\Model\\V4_ShadingAndScreedAndFan\\Results\\temp_1h_A1.prn'
+comfortPRN = 'p:\\_Akquise\\Stuttgart_KnippersHelbig\\TRNLizSim\\Model\\V4_ShadingAndScreedAndFan\\Results\\Comfort_1h.prn'
 
 pickedID = 1
-Occufilter = 1 #occupation filter signal
+Occufilter = 1  # occupation filter signal
 
-#if working from home
-#b18file = "E:\\WORK_IN_PROGRESS\\_Temp\\Model\\BASIS\\BASIS.b18"
-#temperaturePRN = "E:\\WORK_IN_PROGRESS\\_Temp\\Model\\BASIS\\Results\\temp_1h_Z1.prn"
-#comfortPRN = "E:\\WORK_IN_PROGRESS\\_Temp\\Model\\BASIS\\Results\\Comfort_1h.prn"
 
-def checkfile(b18,tempPRN,comfPRN):
+# if working from home
+# b18file = "E:\\WORK_IN_PROGRESS\\_Temp\\Model\\BASIS\\BASIS.b18"
+# temperaturePRN = "E:\\WORK_IN_PROGRESS\\_Temp\\Model\\BASIS\\Results\\temp_1h_Z1.prn"
+# comfortPRN = "E:\\WORK_IN_PROGRESS\\_Temp\\Model\\BASIS\\Results\\Comfort_1h.prn"
+
+def checkfile(b18, tempPRN, comfPRN):
     if not os.path.isfile(b18):
         print("Building file *.b18 not found!")
     if not os.path.isfile(tempPRN):
@@ -25,29 +26,32 @@ def checkfile(b18,tempPRN,comfPRN):
     if not os.path.isfile(comfPRN):
         print("Comfort file *.prn not found!")
 
-checkfile(b18file,temperaturePRN,comfortPRN)
+
+checkfile(b18file, temperaturePRN, comfortPRN)
+
 
 def Readb18(b18file):
-    '''returns comfort pts dictionary {ID:[X,Y,Z]}'''
+    """returns comfort pts dictionary {ID:[X,Y,Z]}"""
     geoposdict = {}
-    b18 = open(b18file,"r")
-    lines =  b18.readlines()
+    b18 = open(b18file, "r")
+    lines = b18.readlines()
     for line in lines:
         if "cgeopos" in line:
             newline = line.split()
-            ptsID = int(newline[1])
+            ptsid = int(newline[1])
             ptsX, ptsY, ptsZ = float(newline[2]), float(newline[3]), float(newline[4])
             pts = []
             pts.append(ptsX), pts.append(ptsY), pts.append(ptsZ)
-            geoposdict[ptsID] = pts
+            geoposdict[ptsid] = pts
     b18.close()
     return geoposdict
 
+
 def ReadTemperature(temperaturePRN):
-    '''return occupation schedule'''
+    """return occupation schedule"""
     occu = []
     hour = []
-    temp = open(temperaturePRN,"r")
+    temp = open(temperaturePRN, "r")
     lines = temp.readlines()
     lines.pop(0)
     lines.pop(0)
@@ -61,88 +65,95 @@ def ReadTemperature(temperaturePRN):
     temp.close()
     return hour, occu
 
+
 def ReadComfort(comfortPRN):
     """returns all comfort values as dictionary {ID:8760values}"""
     comfort = {}
-    comf = open(comfortPRN,"r")
+    comf = open(comfortPRN, "r")
     lines = comf.readlines()
     lines.pop(0)
     lines.pop(0)
-    #setting up blank dictionary
-    dummy = lines[0].split()
-    dummy.pop(0)
-    for id in range(len(dummy)):
-        key = id+1
+    # setting up blank dictionary
+    dummyl = lines[0].split()
+    dummyl.pop(0)
+    for iid in range(len(dummyl)):
+        key = iid + 1
         comfort[key] = []
     for line in lines:
         line = line.split()
         try:
             line.pop(0)
-            for id,value in enumerate(line):
-                dkey = int(id) + 1
-                comfort[dkey].append(float(value))
+            for iid, lvalue in enumerate(line):
+                dkey = int(iid) + 1
+                comfort[dkey].append(float(lvalue))
         except:
             break
     comf.close()
     return comfort
 
-def ComfortOcc(PMV,occupation):
+
+def ComfortOcc(PMV, occupation):
     """return PMV values only during occupation time"""
     PMVOcc = []
-    for id,value in enumerate(occupation):
-        if value == 0:
+    for iid, ovalue in enumerate(occupation):
+        if ovalue == 0:
             PMVOcc.append("")
         else:
-            PMVOcc.append(PMV[id])
+            PMVOcc.append(PMV[iid])
     return PMVOcc
+
 
 def getXY(hours):
     """for plotting"""
-    Xvalue = []
-    Yvalue = []
+    xvalue = []
+    yvalue = []
     year = datetime.date.today().year
-    for item in hours:
-        dummyY = (item-1) % 24 +1
-        Yvalue.append(dummyY)
-        dummyX = (item-1)//24 +1
-        todate = datetime.date(year, 1, 1) + datetime.timedelta(dummyX - 1)
-        Xvalue.append(todate)
-    return Xvalue,Yvalue
+    for hrs in hours:
+        dummyy = (hrs - 1) % 24 + 1
+        yvalue.append(dummyy)
+        dummyx = (hrs - 1) // 24 + 1
+        todate = datetime.date(year, 1, 1) + datetime.timedelta(dummyx - 1)
+        xvalue.append(todate)
+    return xvalue, yvalue
+
 
 def colorAssign(PMV):
-    '''assigining color values based on PMV'''
+    """assigining color values based on PMV"""
     color = []
-    stat = []
-    c_excold, c_cold, c_slcold, c_comf, c_slwarm, c_hot,c_exhot = 0,0,0,0,0,0,0
-    for item in PMV:
-        if item == "":
+    pmvstat = []
+    c_excold, c_cold, c_slcold, c_comf, c_slwarm, c_hot, c_exhot = 0, 0, 0, 0, 0, 0, 0
+    for values in PMV:
+        if values == "":
             color.append(color_Unoccupied)
-            #c_unocc += 1
+            # c_unocc += 1
         else:
-            if item < -3:
-                dummycolor = color_ExtremeCold
+            dcolor = ''
+            if values < -3:
+                dcolor = color_ExtremeCold
                 c_excold += 1
-            elif item >= -3 and item < -1.5:
-                dummycolor = color_Cold
+            elif -3 <= values < -1.5:
+                dcolor = color_Cold
                 c_cold += 1
-            elif item >= -1.5 and item < -0.5:
-                dummycolor = color_SlightlyCold
+            elif -1.5 <= values < -0.5:
+                dcolor = color_SlightlyCold
                 c_slcold += 1
-            elif item >= -0.5 and item <= 0.5:
-                dummycolor = color_Comfortable
+            elif -0.5 <= values <= 0.5:
+                dcolor = color_Comfortable
                 c_comf += 1
-            elif item > 0.5 and item <= 1.5:
-                 dummycolor = color_SlightlyWarm
-                 c_slwarm += 1
-            elif item > 1.5 and item <= 3:
-                dummycolor = color_Hot
+            elif 0.5 < values <= 1.5:
+                dcolor = color_SlightlyWarm
+                c_slwarm += 1
+            elif 1.5 < values <= 3:
+                dcolor = color_Hot
                 c_hot += 1
-            elif item > 3:
-                dummycolor = color_ExtremeHot
+            elif values > 3:
+                dcolor = color_ExtremeHot
                 c_exhot += 1
-            color.append(dummycolor)
-    stat.append(c_excold),stat.append(c_cold),stat.append(c_slcold),stat.append(c_comf),stat.append(c_slwarm),stat.append(c_hot),stat.append(c_exhot)
-    return color, stat
+            color.append(dcolor)
+    pmvstat.append(c_excold), pmvstat.append(c_cold), pmvstat.append(c_slcold), pmvstat.append(c_comf), pmvstat.append(
+        c_slwarm), pmvstat.append(c_hot), pmvstat.append(c_exhot)
+    return color, pmvstat
+
 
 color_Unoccupied = 'rgb(233,233,233)'
 color_ExtremeCold = 'rgb(0,0,255)'
@@ -152,48 +163,49 @@ color_Comfortable = 'rgb(144,245,0)'
 color_SlightlyWarm = 'rgb(255,204,0)'
 color_Hot = 'rgb(255,111,71)'
 color_ExtremeHot = 'rgb(255,0,0)'
-statname = ['Extreme Cold', 'Cold', 'Slightly Cold', 'Comfortable', 'Slightly Warm','Hot','Extreme Hot']
-statcolor = ['rgb(74,0,255)','rgb(0,80,255)','rgb(0,196,255)','rgb(0,255,0)','rgb(255,190,0)','rgb(255,54,0)','rgb(255,255,0']
+statname = ['Extreme Cold', 'Cold', 'Slightly Cold', 'Comfortable', 'Slightly Warm', 'Hot', 'Extreme Hot']
+statcolor = ['rgb(74,0,255)', 'rgb(0,80,255)', 'rgb(0,196,255)', 'rgb(0,255,0)', 'rgb(255,190,0)', 'rgb(255,54,0)',
+             'rgb(255,255,0']
 
 comfortpts = Readb18(b18file)
 hours, occupation = ReadTemperature(temperaturePRN)
-comfortdict = ReadComfort(comfortPRN) #{ID:8760 values}
+comfortdict = ReadComfort(comfortPRN)  # {ID:8760 values}
 
-PMV = comfortdict[pickedID] #wholeyear for selected pts
+PMV = comfortdict[pickedID]  # wholeyear for selected pts
 if Occufilter:
-    PMVresult = ComfortOcc(PMV,occupation) #filtered
+    PMVresult = ComfortOcc(PMV, occupation)  # filtered
 else:
     PMVresult = PMV
-colorPMV,stat = colorAssign(PMVresult)
-xvalues,yvalues= getXY(hours)
+colorPMV, stat = colorAssign(PMVresult)
+xvalues, yvalues = getXY(hours)
 
-#get statistic for 1 pts
+# get statistic for 1 pts
 totalhour = sum(occupation) if Occufilter else 8760
 
 pers = []
 for item in stat:
-    dummy = round(item/totalhour*100,1)
+    dummy = round(item / totalhour * 100, 1)
     pers.append(dummy)
 
-#get statistic for all the points
-statdict = {} #{ptsID: [statistic1,statistic2,...]}
-for ptsID in range(1,len(comfortpts)+1):
+# get statistic for all the points
+statdict = {}  # {ptsID: [statistic1,statistic2,...]}
+for ptsID in range(1, len(comfortpts) + 1):
     PMVID = comfortdict[ptsID]
     if Occufilter:
-        ptsPMV = ComfortOcc(PMVID,occupation) #filtered
+        ptsPMV = ComfortOcc(PMVID, occupation)  # filtered
     else:
         ptsPMV = PMVID
-    dummycolor,dummystat = colorAssign(ptsPMV)
+    dummycolor, dummystat = colorAssign(ptsPMV)
     dummypers = []
     for item in dummystat:
-        value = round(item/totalhour*100,1)
+        value = round(item / totalhour * 100, 1)
         dummypers.append(value)
     statdict[ptsID] = dummypers
 
-#print(statdict.keys())
-print(statdict)
+# print(statdict.keys())
+# print(statdict)
 
-PMV_plotlyScatter(colorPMV,xvalues,yvalues,stat)
-#PMV_BarStatALL(statdict,statname,statcolor)
-#PMV_BarStatID(pers,statname,statcolor,pickedID)
-#PMV_3DStatScatter(statdict,comfortpts)
+PMV_plotlyScatter(colorPMV, xvalues, yvalues, stat, pickedID)
+# PMV_BarStatALL(statdict,statname,statcolor)
+# PMV_BarStatID(pers,statname,statcolor,pickedID)
+# PMV_3DStatScatter(statdict,comfortpts)
